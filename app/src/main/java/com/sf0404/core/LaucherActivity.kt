eac94750b8e0493f2f3473740247eea45124bef0
ThachVo <thach.vo@hubcba.com>
2018-01-09 17:43:02 +0700

    INIT

---
+++ app/src/main/java/com/sf0404/core/LaucherActivity.kt
@@ -0,0 +1,13 @@
+package com.sf0404.core
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import databinding.sample.com.sf0404.a20180108_core_source_mvvm_dagger.R
+
+class LaucherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_laucher)
+    }
+}
